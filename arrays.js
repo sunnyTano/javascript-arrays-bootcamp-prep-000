644ede8d00f206e9d84ae549c7aa4f920151ceb3
Susan Tan <susan.tan198@gmail.com>
2019-01-29 17:02:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,8 @@ function addElementToBeginningOfArray(arr, el){
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, el){
-    return arr.unshift(el);
+    arr.unshift(el)
+    return arr;
 }
 
 function addElementToEndOfArray(arr, el){
